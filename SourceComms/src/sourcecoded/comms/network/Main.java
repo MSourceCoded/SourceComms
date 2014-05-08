90161be69ff543da40bf4221e901bd23b28817a5
MrSourceCoded <sourcecodedcreations@gmail.com>
2014-05-08 19:20:34 +0800

    Test Commit

--- SourceComms/src/sourcecoded/comms/network/Main.java
+++ SourceComms/src/sourcecoded/comms/network/Main.java
@@ -5,7 +5,6 @@ import sourcecoded.comms.eventsystem.SourceCommsEvent;
 import sourcecoded.comms.eventsystem.event.EventPacketHandled;
 import sourcecoded.comms.exception.ErrorHandler;
 import sourcecoded.comms.network.packets.Pkt0x00Ping;
-import sourcecoded.comms.network.packets.Pkt1x00Player;
 import sourcecoded.comms.socket.SourceCommsClient;
 import sourcecoded.comms.socket.SourceCommsServer;
 
@@ -33,11 +32,5 @@ public class Main {
 	
 	@SourceCommsEvent
 	public void onPacketHandled(EventPacketHandled e) {
-		if (e.getPacket() instanceof Pkt1x00Player) {
-			System.err.println("This should only trigger on Pkt1x00");
-			System.err.println(((Pkt1x00Player)e.getPacket()).target);
-		} else {
-			System.err.println("Not a player packet");
-		}
 	}
 }
